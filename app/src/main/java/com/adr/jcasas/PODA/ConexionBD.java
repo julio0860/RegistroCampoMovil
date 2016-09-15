ef2099027806b126534f827660818d5e9cbfd11e
julio0860 <julio0860@hotmail.com>
2016-09-15 10:45:46 -0500

    cambio2

--- app/src/main/java/com/adr/jcasas/PODA/ConexionBD.java
+++ app/src/main/java/com/adr/jcasas/PODA/ConexionBD.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//Hola Mundo
 public class ConexionBD {
 
     private static ConexionBD instance = null;
